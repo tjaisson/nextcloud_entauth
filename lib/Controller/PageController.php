6d39fe76d990a3788965378df875d17280f67058
tjaisson <thomas.jaisson@ac-paris.fr>
2019-02-07 18:06:17 +0100

    test crypt

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -2,31 +2,36 @@
 namespace OCA\EntcoreAuth\Controller;
 
 use OCP\IRequest;
-use OCP\AppFramework\Http\TemplateResponse;
+use OCP\AppFramework\Http\Template\PublicTemplateResponse;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\Controller;
+use OCA\EntcoreAuth\Crypt;
 
 class PageController extends Controller {
 	private $userId;
-
-	public function __construct($AppName, IRequest $request, $UserId){
+	private $Crypt;
+	
+	public function __construct($AppName,
+	    IRequest $request,
+	    $UserId,
+	    Crypt $Crypt) {
 		parent::__construct($AppName, $request);
 		$this->userId = $UserId;
+		$this->Crypt = $Crypt;
 	}
 
 	/**
-	 * CAUTION: the @Stuff turns off security checks; for this page no admin is
-	 *          required and no CSRF check. If you don't know what CSRF is, read
-	 *          it up in the docs or you might create a security hole. This is
-	 *          basically the only required method to add this exemption, don't
-	 *          add it to any other method if you don't exactly know what it does
-	 *
 	 * @NoAdminRequired
 	 * @PublicPage
 	 * @NoCSRFRequired
 	 */
 	public function index() {
-		return new TemplateResponse('entcoreauth', 'index');  // templates/index.php
+	    $crpt = $this->Crypt->seal('7c7a2376-93c8-4b40-8d22-532eae2f407c');
+	    $decrpt = $this->Crypt->open($crpt);
+	    $template = new PublicTemplateResponse($this->appName, 'index', ['crpt' => $crpt, 'decrpt' => $decrpt]);
+	    $template->setHeaderTitle('Authentification ENT');
+	    $template->setHeaderDetails('Associer un compte');
+	    return $template;
 	}
 
 }
