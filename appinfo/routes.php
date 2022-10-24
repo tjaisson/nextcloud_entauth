3a882a85202c531f82d79a4f9287537d10503d3e
tjaisson <thomas.jaisson@ac-paris.fr>
2022-10-24 21:39:54 +0000

    update various files

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -1,12 +1,8 @@
 <?php
-/**
- * Create your routes in here. The name is the lowercase name of the controller
- * without the controller part, the stuff after the hash is the method.
- * e.g. page#index -> OCA\EntcoreAuth\Controller\PageController->index()
- *
- * The controller class has to be registered in the application.php file since
- * it's instantiated in there
- */
+declare(strict_types=1);
+// SPDX-FileCopyrightText: Thomas Jaisson <thomas.jaisson@ac-paris.fr>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 return [
     'routes' => [
         ['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
