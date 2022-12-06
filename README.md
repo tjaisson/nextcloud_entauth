0ca8e1a3300c2760ed7ed319a517766a35c25398
tjaisson <thomas.jaisson@ac-paris.fr>
2022-12-06 13:07:47 +0000

    Add config sample

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ SPDX-License-Identifier: CC0-1.0
 
 # Ent Auth
 Place this app in **nextcloud/apps/**
+
+## Configuration
+La configuration de l'appli se fait dans un fichier **entconf.php** à placer
+dans le répertoire **config** de nextcloud.  
+Le fichier d'exemple **entconf.sample.php** explique le format de ce fichier.
---
+++ entconf.sample.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Exemple de fichier de configuration
+ * Copiez ce fichier dans le répertoire config de nextcloud
+ * en le renommant entconf.php
+ * 
+ * Les clefs 'ent1', 'ent2' sont utilisées pour former les url
+ * de connexion
+ * 
+ * Le champ 'dbId' est un entier qui doit être différent pour chaque ent.
+ * Le 'dbId' d'un ent ne doit pas être changé par la suite
+ * 
+ * Le type Entcore est le seul actuellement implémenté.
+ */
+return [
+  'ent1' => [
+    'name' => 'Ent1 display name',
+    'dbId' => 1,
+    'host' => 'ent1.host.xyz',
+    'appId' => 'the app id 1',
+    'secret' => 'the secret 1',
+    'type' => 'Entcore'
+  ],
+  'ent2' => [
+    'name' => 'Ent2 display name',
+    'dbId' => 2,
+    'host' => 'ent2.host.xyz',
+    'appId' => 'the app id 2',
+    'secret' => 'the secret 2',
+    'type' => 'Entcore'
+  ]
+];
